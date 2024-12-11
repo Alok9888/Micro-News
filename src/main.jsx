1e789ea7a12d3dd4f0f7ef32e5567bb4f7bcbe36
hkgonebad <harry007007@gmail.com>
2024-12-11 19:40:46 +0530

    try: redirect configs

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.scss";
 // import bootstrap js
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
@@ -8,8 +8,8 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
@@ -6,5 +7,12 @@ export default defineConfig({
   plugins: [react()],
   alias: {
     "@content": "/src/content",
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
   },
 });
