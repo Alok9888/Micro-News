090b8d10e184f04be6234b505b85184b8f98a647
aerodeval <sydney223as@gmail.com>
2024-12-27 16:02:50 +0530

    add: Added captions to gallery images

--- src/content/images.js
+++ src/content/images.js
@@ -1,261 +1,413 @@
-const images = [
-  { src: `63.jpg`, title: "Image 63", category: "default" },
+const images=[
   {
-    src: "1.jpg",
-    title: "Republic Day celebrations across our office spaces",
-    category: "default",
-    null: null,
+    src: "63.jpg",
+    title: "Our leaders at the Reliance Green Diwali Utsav 2024",
+    category: "MAKE THIS THE FIRST IMAGE IN THE GALLERY"
   },
   {
-    src: "2.jpg",
-    title: "Republic Day celebrations across our office spaces",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "3.jpg",
-    title: "Republic Day celebrations across our office spaces",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "4.jpg",
-    title: "Republic Day celebrations across our office spaces",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "5.jpg",
-    title: "Republic Day celebrations at Jio World Centre",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "6.jpg",
-    title: "Republic Day celebrations at Jio Institute",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "7.jpg",
-    title: "Republic Day celebrations at Dhirubhai Ambani Square, NMACC",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "8.jpg",
-    title: "Republic Day celebrations at Jio",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "9.jpg",
-    title: "Republic Day parade at Reliance",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "10.jpg",
-    title: "Glimpses from the Photography Day workshop at RCP, Navi Mumbai",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "11.jpg",
-    title: "Glimpses from the Photography Day workshop at RCP, Navi Mumbai",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "12.jpg",
-    title: "At the International Women’s Day walkathon at RCP, Navi Mumbai",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "13.jpg",
-    title: "A colleague enjoying volleyball",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "14.jpg",
-    title: "A colleague enjoying volleyball",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "15.jpg",
-    title: "Colleagues enjoying foosball",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "16.jpg",
-    title: "A Reliance colleague and his family exploring the RCP campus",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "17.jpg",
-    title: "Sustainable Fashion Show at RCP, Navi Mumbai, organised to mark Environment Day celebrations",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "18.jpg",
-    title: "Sustainable Fashion Show at RCP, Navi Mumbai, organised to mark Environment Day celebrations",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "19.jpg",
-    title: "Colleagues with their certificates following a blood donation drive",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "20.jpg",
-    title: "Colleagues playing badminton",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "21.jpg",
-    title: "At the Jio Mumbai Sustainability Cyclothon",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "22.jpg",
-    title: "Independence Day celebrations at Jio",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "23.jpg",
-    title: "Some of our colleagues with a furry friend on campus",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "24.jpg",
-    title: "Some of our colleagues with a furry friend on campus",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "25.jpg",
-    title: "Upcycling activities at RCP, Navi Mumbai",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "26.jpg",
-    title: "Colleagues celebrate Onam",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "27.jpg",
-    title: "Colleagues celebrate Onam",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "28.jpg",
-    title: "Flower rangoli as part of Onam celebrations",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "29.jpg",
-    title: "A colleague from Reliance Jewels, Viviana Mall, Thane, is all smiles during the festive season",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "30.jpg",
-    title: "Republic Day celebrations at RCP, Navi Mumbai",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "31.jpg",
-    title: "Independence Day celebrations",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "32.jpg",
-    title: "Republic Day celebrations at Reliance Life Sciences, Navi Mumbai",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "33.jpg",
-    title: "A colleague enjoying a swim at RCP, Navi Mumbai",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "34.jpg",
-    title: "Colleagues at the R-Swasthya walkathon",
-    category: "default",
-    null: null,
-  },
-  {
-    src: "35.jpg",
-    title: "Colleagues at the R-Swasthya walkathon",
-    category: "default",
-    null: null,
-  },
-  { src: `36.jpg`, title: "Image 36", category: "default" },
-  // { src: `37.jpg`, title: "Image 37", category: "default" },
-  { src: `38.jpg`, title: "Image 38", category: "default" },
-  { src: `39.jpg`, title: "Image 39", category: "default" },
-  { src: `40.jpg`, title: "Image 40", category: "default" },
-  { src: `41.jpg`, title: "Image 41", category: "default" },
-  { src: `42.jpg`, title: "Image 42", category: "default" },
-  { src: `43.jpg`, title: "Image 43", category: "default" },
-  { src: `44.jpg`, title: "Image 44", category: "default" },
-  { src: `45.jpg`, title: "Image 45", category: "default" },
-  { src: `46.jpg`, title: "Image 46", category: "default" },
-  { src: `47.jpg`, title: "Image 47", category: "default" },
-  { src: `48.jpg`, title: "Image 48", category: "default" },
-  { src: `49.jpg`, title: "Image 49", category: "default" },
-  { src: `50.jpg`, title: "Image 50", category: "default" },
-  { src: `51.jpg`, title: "Image 51", category: "default" },
-  { src: `52.jpg`, title: "Image 52", category: "default" },
-  { src: `53.jpg`, title: "Image 53", category: "default" },
-  { src: `54.jpg`, title: "Image 54", category: "default" },
-  { src: `55.jpg`, title: "Image 55", category: "default" },
-  { src: `56.jpg`, title: "Image 56", category: "default" },
-  { src: `57.jpg`, title: "Image 57", category: "default" },
-  { src: `58.jpg`, title: "Image 58", category: "default" },
-  { src: `59.jpg`, title: "Image 59", category: "default" },
-  { src: `60.jpg`, title: "Image 60", category: "default" },
-  { src: `61.jpg`, title: "Image 61", category: "default" },
-  { src: `62.jpg`, title: "Image 62", category: "default" },
+  src: "1.jpg",
+  title: "Republic Day celebrations across our office spaces",
+  category: "default"
+},
+{
+  src: "2.jpg",
+  title: "Republic Day celebrations across our office spaces",
+  category: "default"
+},
+{
+  src: "3.jpg",
+  title: "Republic Day celebrations across our office spaces",
+  category: "default"
+},
+{
+  src: "4.jpg",
+  title: "Republic Day celebrations across our office spaces",
+  category: "default"
+},
+{
+  src: "5.jpg",
+  title: "Republic Day celebrations at Jio World Centre",
+  category: "default"
+},
+{
+  src: "6.jpg",
+  title: "Republic Day celebrations at Jio Institute",
+  category: "default"
+},
+{
+  src: "7.jpg",
+  title: "Republic Day celebrations at Dhirubhai Ambani Square, NMACC",
+  category: "default"
+},
+{
+  src: "8.jpg",
+  title: "Republic Day celebrations at Jio",
+  category: "default"
+},
+{
+  src: "9.jpg",
+  title: "Republic Day parade at Reliance",
+  category: "default"
+},
+{
+  src: "10.jpg",
+  title: "Glimpses from the Photography Day workshop at RCP, Navi Mumbai",
+  category: "default"
+},
+{
+  src: "11.jpg",
+  title: "Glimpses from the Photography Day workshop at RCP, Navi Mumbai",
+  category: "default"
+},
+{
+  src: "12.jpg",
+  title: "At the International Women’s Day walkathon at RCP, Navi Mumbai",
+  category: "default"
+},
+{
+  src: "13.jpg",
+  title: "A colleague enjoying volleyball",
+  category: "default"
+},
+{
+  src: "14.jpg",
+  title: "A colleague enjoying volleyball",
+  category: "default"
+},
+{
+  src: "15.jpg",
+  title: "Colleagues enjoying foosball",
+  category: "default"
+},
+{
+  src: "16.jpg",
+  title: "A Reliance colleague and his family exploring the RCP campus",
+  category: "default"
+},
+{
+  src: "17.jpg",
+  title: "Sustainable Fashion Show at RCP, Navi Mumbai, organised to mark Environment Day celebrations",
+  category: "default"
+},
+{
+  src: "18.jpg",
+  title: "Sustainable Fashion Show at RCP, Navi Mumbai, organised to mark Environment Day celebrations",
+  category: "default"
+},
+{
+  src: "19.jpg",
+  title: "Colleagues with their certificates following a blood donation drive",
+  category: "default"
+},
+{
+  src: "20.jpg",
+  title: "Colleagues playing badminton",
+  category: "default"
+},
+{
+  src: "21.jpg",
+  title: "At the Jio Mumbai Sustainability Cyclothon",
+  category: "default"
+},
+{
+  src: "22.jpg",
+  title: "Independence Day celebrations at Jio",
+  category: "default"
+},
+{
+  src: "23.jpg",
+  title: "Some of our colleagues with a furry friend on campus",
+  category: "default"
+},
+{
+  src: "24.jpg",
+  title: "Some of our colleagues with a furry friend on campus",
+  category: "default"
+},
+{
+  src: "25.jpg",
+  title: "Upcycling activities at RCP, Navi Mumbai",
+  category: "default"
+},
+{
+  src: "26.jpg",
+  title: "Colleagues celebrate Onam",
+  category: "default"
+},
+{
+  src: "27.jpg",
+  title: "Colleagues celebrate Onam",
+  category: "default"
+},
+{
+  src: "28.jpg",
+  title: "Flower rangoli as part of Onam celebrations",
+  category: "default"
+},
+{
+  src: "29.jpg",
+  title: "A colleague from Reliance Jewels, Viviana Mall, Thane, is all smiles during the festive season",
+  category: "default"
+},
+{
+  src: "30.jpg",
+  title: "Republic Day celebrations at RCP, Navi Mumbai",
+  category: "default"
+},
+{
+  src: "31.jpg",
+  title: "Independence Day celebrations",
+  category: "default"
+},
+{
+  src: "32.jpg",
+  title: "Republic Day celebrations at Reliance Life Sciences, Navi Mumbai",
+  category: "default"
+},
+{
+  src: "33.jpg",
+  title: "A colleague enjoying a swim at RCP, Navi Mumbai",
+  category: "default"
+},
+{
+  src: "34.jpg",
+  title: "Colleagues at the R-Swasthya walkathon",
+  category: "default"
+},
+{
+  src: "35.jpg",
+  title: "Colleagues at the R-Swasthya walkathon",
+  category: "default"
+},
+{
+  src: "36.jpg",
+  title: "Reliance leaders with colleagues at the Green Diwali Utsav",
+  category: "default"
+},
+// {
+//   src: "37.jpg",
+//   title: "",
+//   category: "DELETE REPEATING IMAGE"
+// },
+{
+  src: "38.jpg",
+  title: "A Retail Sitara contestant",
+  category: "default"
+},
+{
+  src: "39.jpg",
+  title: "A colleague at the NW18 booth at the Reliance Green Diwali Utsav",
+  category: "default"
+},
+{
+  src: "40.jpg",
+  title: "A tree plantation drive at PMD on World Environment Day",
+  category: "default"
+},
+{
+  src: "41.jpg",
+  title: "At the EmpowHER Rural Mela",
+  category: "default"
+},
+{
+  src: "42.jpg",
+  title: "At the HMD-PT Long Service Awards",
+  category: "default"
+},
+{
+  src: "43.jpg",
+  title: "Bihu celebrations at Jio",
+  category: "default"
+},
+{
+  src: "44.jpg",
+  title: "Colleagues at the Reliance Green Diwali Utsav",
+  category: "default"
+},
+{
+  src: "45.jpg",
+  title: "Colleagues at Anant and Radhika's wedding reception",
+  category: "default"
+},
+{
+  src: "46.jpg",
+  title: "Colleagues at Anant and Radhika's wedding reception",
+  category: "default"
+},
+{
+  src: "47.jpg",
+  title: "Colleagues at Anant and Radhika's wedding reception",
+  category: "default"
+},
+{
+  src: "48.jpg",
+  title: "Colleagues at the NW18 booth at the Reliance Green Diwali Utsav",
+  category: "default"
+},
+{
+  src: "49.jpg",
+  title: "Colleagues at the Reliance Green Diwali Utsav",
+  category: "default"
+},
+{
+  src: "50.jpg",
+  title: "Colleagues at the Reliance Green Diwali Utsav",
+  category: "default"
+},
+{
+  src: "51.jpg",
+  title: "At the Monsoon Music Masti event at RCP, Navi Mumbai",
+  category: "default"
+},
+{
+  src: "52.jpg",
+  title: "Hazira Manufacturing Division's blood donation drive",
+  category: "default"
+},
+{
+  src: "53.jpg",
+  title: "Independence Day celebrations at Barabanki",
+  category: "default"
+},
+{
+  src: "54.jpg",
+  title: "Independence Day celebrations at Dahej Manufacturing Division",
+  category: "default"
+},
+{
+  src: "55.jpg",
+  title: "Independence Day celebrations at Hazira Manufacturing Division",
+  category: "default"
+},
+{
+  src: "56.jpg",
+  title: "Independence Day celebrations at Jamnagar Manufacturing Division",
+  category: "default"
+},
+{
+  src: "57.jpg",
+  title: "Independence Day celebrations at Silvassa Manufacturing Division",
+  category: "default"
+},
+{
+  src: "58.jpg",
+  title: "Independence Day celebrations at Vadodara Manufacturing Division",
+  category: "default"
+},
+{
+  src: "59.jpg",
+  title: "International Yoga Day at Dahej",
+  category: "default"
+},
+{
+  src: "60.jpg",
+  title: "International Yoga Day at Hazira",
+  category: "default"
+},
+{
+  src: "61.jpg",
+  title: "IWD celebrations at Vadodara Manufacturing Division",
+  category: "default"
+},
+{
+  src: "62.jpg",
+  title: "Olympic Day celebrations by Reliance Foundation in Mumbai",
+  category: "default"
+},
 
-  { src: `64.jpg`, title: "Image 64", category: "default" },
-  { src: `65.jpg`, title: "Image 65", category: "default" },
-  { src: `66.jpg`, title: "Image 66", category: "default" },
-  { src: `67.jpg`, title: "Image 67", category: "default" },
-  { src: `68.jpg`, title: "Image 68", category: "default" },
-  { src: `69.jpg`, title: "Image 69", category: "default" },
-  { src: `70.jpg`, title: "Image 70", category: "default" },
-  { src: `71.jpg`, title: "Image 71", category: "default" },
-  { src: `72.jpg`, title: "Image 72", category: "default" },
-  { src: `73.jpg`, title: "Image 73", category: "default" },
-  { src: `74.jpg`, title: "Image 74", category: "default" },
-  { src: `75.jpg`, title: "Image 75", category: "default" },
-  // { src: `76.jpg`, title: "Image 76", category: "default" },
-  { src: `77.jpg`, title: "Image 77", category: "default" },
-  { src: `78.jpg`, title: "Image 78", category: "default" },
-  { src: `79.jpg`, title: "Image 79", category: "default" },
-  { src: `80.jpg`, title: "Image 80", category: "default" },
-  { src: `81.jpg`, title: "Image 81", category: "default" },
-];
+{
+  src: "64.jpg",
+  title: "Participants at a gardening workshop at Hazira Manufacturing Division",
+  category: "default"
+},
+{
+  src: "65.jpg",
+  title: "Patalganga's Polyster Division celebrates Yoga Day",
+  category: "default"
+},
+{
+  src: "66.jpg",
+  title: "R&M team celebrates International Women's Day",
+  category: "default"
+},
+{
+  src: "67.jpg",
+  title: "The K-9 dog squad at RCP",
+  category: "default"
+},
+{
+  src: "68.jpg",
+  title: "Reliance leaders with colleagues at the Green Diwali Utsav",
+  category: "default"
+},
+{
+  src: "69.jpg",
+  title: "Republic Day celebrations at Reliance Life Sciences, Navi Mumbai",
+  category: "default"
+},
+{
+  src: "70.jpg",
+  title: "Retail Employee Day celebrations",
+  category: "default"
+},
+{
+  src: "71.jpg",
+  title: "Retail Employee Day celebrations",
+  category: "default"
+},
+{
+  src: "72.jpg",
+  title: "Retail Employee Day celebrations",
+  category: "default"
+},
+{
+  src: "73.jpg",
+  title: "Retail Sitara organisers with winners and finalists",
+  category: "DELETE REPEATING IMAGE"
+},
+{
+  src: "74.jpg",
+  title: "Retail Sitara winners and finalists with jury members",
+  category: "default"
+},
+{
+  src: "75.jpg",
+  title: "Six-time Olympian Shiva Keshavan with children at Olympic Day celebrations by Reliance Foundation",
+  category: "default"
+},
+// {
+//   src: "76.jpg",
+//   title: "",
+//   category: "REMOVE THIS"
+// },
+{
+  src: "77.jpg",
+  title: "Team Jio-bp celebrates International Women's Day",
+  category: "default"
+},
+{
+  src: "78.jpg",
+  title: "Jury members at Retail Sitara",
+  category: "default"
+},
+{
+  src: "79.jpg",
+  title: "Tree plantation drive at Dahej Manufacturing Division",
+  category: "default"
+},
+{
+  src: "80.jpg",
+  title: "Ugadi celebrations at Jio Andhra Pradesh",
+  category: "default"
+},
+{
+  src: "81.jpg",
+  title: "Women's Day Celebrations at Jio World Centre",
+  category: "default"
+}]
+
+
+
+
 
 export default images;
