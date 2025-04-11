3004310da7db90623276ada23990ef7d08b1dc44
hkgonebad <harry007007@gmail.com>
2025-04-12 00:27:11 +0530

    feat: enhance Footer component with text alignment and creator attribution for improved user engagement

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
 const Footer = () => {
   return (
     <footer>
-      <div className="container">
+      <div className="container text-center">
         <p>2024-25 Micro News Portal. All Rights Reserved.</p>
+
+        <p>
+          Created with ❤️ by <a href="https://github.com/hkgonebad/micro-news-portal">Hk</a>
+        </p>
       </div>
     </footer>
   );
